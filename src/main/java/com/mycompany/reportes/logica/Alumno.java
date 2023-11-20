6357cbac1ef8197d6f22165588f0a96912f6ffef
DamianBlock <damianfrt25@gmail.com>
2023-11-20 17:57:43 -0300

    prueba desde netbeans eclipse

--- src/main/java/com/mycompany/reportes/logica/Alumno.java
+++ src/main/java/com/mycompany/reportes/logica/Alumno.java
@@ -1,13 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.reportes.logica;
 
-/**
- *
- * @author damia
- */
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Alumno {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    
+    @Basic
+    private String nombre;
+    private String apellido;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fecha_Nac;
+
+    public Alumno() {
+    }
+
+    public Alumno(int id, String nombre, String apellido, Date fecha_Nac) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecha_Nac = fecha_Nac;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFecha_Nac() {
+        return fecha_Nac;
+    }
+
+    public void setFecha_Nac(Date fecha_Nac) {
+        this.fecha_Nac = fecha_Nac;
+    }
+    
     
 }
